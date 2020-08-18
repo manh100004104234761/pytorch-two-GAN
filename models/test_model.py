115da7d04690f667fe14509611f48900c64e7ac6
tuanlh <tuanlh@kikaitech.io>
2020-08-18 13:54:16 +0700

    update

--- models/test_model.py
+++ models/test_model.py
@@ -12,16 +12,16 @@ class TestModel(BaseModel):
     def initialize(self, opt):
         assert(not opt.isTrain)
         BaseModel.initialize(self, opt)
-        self.netG = networks.define_G(opt.input_nc, opt.output_nc,
-                                      opt.ngf, opt.which_model_netG,
-                                      opt.norm, not opt.no_dropout,
-                                      opt.init_type,
-                                      self.gpu_ids)
-        which_epoch = opt.which_epoch
-        self.load_network(self.netG, 'G', which_epoch)
-
+        self.seg_netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf,
+                                          opt.which_model_netG, opt.norm, not opt.no_dropout, opt.init_type, self.gpu_ids)
+        self.detec_netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf,
+                                            opt.which_model_netG, opt.norm, not opt.no_dropout, opt.init_type, self.gpu_ids)
+        self.load_network(self.seg_netG, 'G', 'seg_latest')
+        self.load_network(self.detec_netG, 'G', 'detec_latest')
+        print('Warning: continue_train is not supported')
         print('---------- Networks initialized -------------')
-        networks.print_network(self.netG)
+        networks.print_network(self.seg_netG)
+        networks.print_network(self.detec_netG)
         print('-----------------------------------------------')
 
     def set_input(self, input):
@@ -43,4 +43,7 @@ class TestModel(BaseModel):
     def get_current_visuals(self):
         real_A = util.tensor2im(self.real_A.data)
         fake_B = util.tensor2im(self.fake_B.data)
-        return OrderedDict([('real_A', real_A), ('fake_B', fake_B)])
+        fake_D = util.tensor2im(self.fake_D.data)
+        fake_C = util.tensor2im(self.fake_C.data)
+        return OrderedDict([('real_A', real_A), ('fake_B', fake_B), ('fake_C', fake_C),
+                            ('fake_D', fake_D)])
